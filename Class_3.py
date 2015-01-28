963e2c08227c33ed96b3c2e4b4a8c3b11419c571
muxiyang <muxi.yang@duke.edu>
2015-01-28 15:25:34 -0500

    Class_3

---
+++ Class_3.py
@@ -0,0 +1,7 @@
+from pandas import Series, DataFrame 
+import pandas as pd 
+import numpy as np 
+
+main_dir="/Users/newlife-cassie/Desktop/Data"
+git_dir="/Users/newlife-cassie/Desktop/PUBPOL590"
+
